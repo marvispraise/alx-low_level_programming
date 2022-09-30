4f1e28eb5bf409f36e81d3d621819542aa63d905
Marvelous Praise <marvelousopraise@gmail.com>
2022-09-30 04:28:07 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -8,9 +7,13 @@
 * Return: 0
 */
 
-int main(int argc, char *argv[])
+void main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc * 0]);
+	int i;
 
-	return (0);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s ", argv[i]);
+	}
+	printf("\n");
 }
