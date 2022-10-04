d7304f2f8f75b2c47277516fa44ebfd3776f00ac
Marvelous Praise <marvelousopraise@gmail.com>
2022-10-04 02:13:25 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+* File: main.h
+* Desc: Header file containing prototypes for all functions
+* used in the 0x0A-malloc_free directory.
+*/
+
+char *create_array(unsigned int size, char c);
+
+#endif
